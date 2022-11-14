cb2db0f7440ce59266df6d9ad7d538b18b0bb640
김근부 <rmsqn77@ejbt.co.kr>
2022-11-14 11:15:21 +0900

    11/11 commit

--- src/main/java/algorithm/no5/No15596.java
+++ src/main/java/algorithm/no5/No15596.java
@@ -1,2 +1,11 @@
-package algorithm.no5;public class No15596 {
+package algorithm.no5;
+
+public class No15596 {
+    public long sum(int[] a) {
+        long b = 0;
+        for(int i=0; i < a.length; i++) {
+            b += a[i];
+        }
+        return b;
+    }
 }
--- src/main/java/algorithm/no5/No4673.java
+++ src/main/java/algorithm/no5/No4673.java
@@ -1,2 +1,25 @@
-package algorithm.no5;public class No4673 {
+package algorithm.no5;
+
+public class No4673 {
+    public static void main(String[] args) {
+        boolean[] arr = new boolean[10001];
+        for(int i=1; i < 10001; i++) {
+            if(d(i) < 10001) {
+                arr[d(i)] = true;
+            }
+        }
+        for(int i=1; i < arr.length; i++) {
+            if(!arr[i]) {
+                System.out.println(i);
+            }
+        }
+    }
+    public static int d(int n) {
+        int sum=n; // n = 1234
+        while(n != 0) {
+            sum = sum + (n % 10);
+            n = n/10;
+        }
+        return sum;
+    }
 }
